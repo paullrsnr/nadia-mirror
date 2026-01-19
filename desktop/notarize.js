ff320951ef47a0b127ce5adcbd0155288f39e393
paullrsnr <95188132+paullrsnr@users.noreply.github.com>
2026-01-19 05:22:22 +0100

    prepare notarisation, build windows

---
+++ desktop/notarize.js
@@ -0,0 +1,19 @@
+const { notarize } = require("@electron/notarize");
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+
+  if (electronPlatformName !== "darwin") {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    tool: "notarytool",
+    appPath: `${appOutDir}/${appName}.app`,
+    appleApiKey: process.env.APPLE_API_KEY_PATH,
+    appleApiKeyId: process.env.APPLE_API_KEY_ID,
+    appleApiIssuer: process.env.APPLE_API_ISSUER,
+  });
+};
