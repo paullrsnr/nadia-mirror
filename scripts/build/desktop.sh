df2777b8e3abc16f5f9c271e021f6d23e198cf4d
paullrsnr <95188132+paullrsnr@users.noreply.github.com>
2026-01-20 20:52:41 +0100

    clean cache

--- scripts/build/desktop.sh
+++ scripts/build/desktop.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "💻 Building desktop (Electron app)"
+echo "🧹 Cleaning npm cache"
+npm cache clean --force
 
+echo "💻 Building desktop (Electron app)"
 cd desktop
 npm install
 npm run build
