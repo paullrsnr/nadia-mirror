2c7c06a1abe620aad0665c650ef3b124e0e985f4
paullrsnr <95188132+paullrsnr@users.noreply.github.com>
2026-01-20 21:28:37 +0100

    pre script in context

--- scripts/package/macOs.sh
+++ scripts/package/macOs.sh
@@ -2,6 +2,14 @@
 set -e
 echo "📦 Packaging macOS installer"
 
+# Node / npm
+# shellcheck disable=SC1091
+source "$(dirname "$0")/../setup/nodeEnv.sh"
+
+# Apple notarization
+# shellcheck disable=SC1091
+source "$(dirname "$0")/../setup/prePackageMacOs.sh"
+
 cd desktop
 npm install
 mkdir -p ../backend/dist/win # create empty win folder to avoid errors
